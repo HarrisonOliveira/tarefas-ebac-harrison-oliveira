080c593402b5aea2a3102da7709934ebd7ff6cde
Harrison Gessner <harrison.aso@hotmail.com>
2024-05-11 18:46:57 -0500

    Resposta para atividade do Modulo 10 - Controles de Fluxo

---
+++ mod10/tarefa_controleDeFluxo/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite a sua nota: ");
+
+        int nota = s.nextInt();
+
+        if (nota >= 7) System.out.println("Você foi aprovado, parabêns!");
+        else if (nota >= 5) System.out.println("Você está de recuperação.");
+        else System.out.println("Você está reprovado. ");
+    }
+}
