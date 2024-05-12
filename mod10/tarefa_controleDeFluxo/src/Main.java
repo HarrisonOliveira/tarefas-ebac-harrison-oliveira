77727f6a63006cea7450740186c56b1cb4c211b4
Harrison Gessner <harrison.aso@hotmail.com>
2024-05-12 15:23:59 -0500

    [Refactor] - Solicitação de entrada e calculo de média

--- mod10/tarefa_controleDeFluxo/src/Main.java
+++ mod10/tarefa_controleDeFluxo/src/Main.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 //Tarefa realizada com a versão do JAVA 22
 
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
 
-        System.out.println("Digite a sua primeira notaz: ");
-        byte nota1 = s.nextByte();
+    byte contador = 1;
+    float nota = 0f;
 
-        System.out.println("Digite a sua quinta nota: ");
-        byte nota2 = s.nextByte();
 
-        System.out.println("Digite a sua terceira nota: ");
-        byte nota3 = s.nextByte();
 
-        System.out.println("Digite a sua quarta nota: ");
-        byte nota4 = s.nextByte();
-
-        int nota = (nota1 + nota2 + nota3 + nota4) / 4;
-
-        if (nota >= 7) System.out.println(STR."Sua nota média foi \{nota}. Você foi aprovado, parabêns!");
-        else if (nota >= 5) System.out.println(STR."Sua nota média foi \{nota}. Você está de recuperação." );
-        else System.out.println(STR."Sua nota média foi \{nota}. Você está reprovado. ");
+    do {
+        System.out.println(STR."Insira sua nota \{contador}: ");
+        nota = s.nextInt() + nota;
+        contador++;
     }
+    while (contador <= 4); {
+        nota = nota / 4;
+        if (nota >= 7) System.out.println(STR."Sua nota média foi de \{nota} pontos. Você foi aprovado, parabêns!");
+        else if (nota >= 5) System.out.println(STR."Sua nota média foi de \{nota} pontos. Você está de recuperação.");
+        else System.out.println(STR."Sua nota média foi de \{nota} pontos. Você está reprovado. ");
+    }
+
+
 }
