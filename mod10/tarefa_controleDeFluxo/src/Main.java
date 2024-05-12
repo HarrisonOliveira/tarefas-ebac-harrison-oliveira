bc2640e7a5ccd8812ca28490d27309f424879b4b
Harrison Gessner <harrison.aso@hotmail.com>
2024-05-11 19:17:13 -0500

    [Fix] - Resposta para atividade do Modulo 10 - Controles de Fluxo

--- mod10/tarefa_controleDeFluxo/src/Main.java
+++ mod10/tarefa_controleDeFluxo/src/Main.java
@@ -6,12 +6,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Digite a sua nota: ");
 
-        byte nota = s.nextByte();
+        System.out.println("Digite a sua primeira notaz: ");
+        byte nota1 = s.nextByte();
 
-        if (nota >= 7) System.out.println("Você foi aprovado, parabêns!");
-        else if (nota >= 5) System.out.println("Você está de recuperação.");
-        else System.out.println("Você está reprovado. ");
+        System.out.println("Digite a sua quinta nota: ");
+        byte nota2 = s.nextByte();
+
+        System.out.println("Digite a sua terceira nota: ");
+        byte nota3 = s.nextByte();
+
+        System.out.println("Digite a sua quarta nota: ");
+        byte nota4 = s.nextByte();
+
+        int nota = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (nota >= 7) System.out.println(STR."Sua nota média foi \{nota}. Você foi aprovado, parabêns!");
+        else if (nota >= 5) System.out.println(STR."Sua nota média foi \{nota}. Você está de recuperação." );
+        else System.out.println(STR."Sua nota média foi \{nota}. Você está reprovado. ");
     }
 }
