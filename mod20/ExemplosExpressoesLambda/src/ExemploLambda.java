4b24c5e0b13bc9b9eb5144aa4a1a0fd1617c2b86
Harrison Oliveira <harrison.aso@hotmail.com>
2025-06-19 07:30:13 -0500

    Adicionado Exemplos do Modulo 20

--- mod20/ExemplosExpressoesLambda/src/ExemploLambda.java
+++ mod20/ExemplosExpressoesLambda/src/ExemploLambda.java
@@ -1,11 +1,27 @@
+import java.util.function.BiFunction;
 import java.util.function.IntBinaryOperator;
 
 public class ExemploLambda {
     public static void main(String[] args) {
+
+        // Lambda utilizando IntBinaryOperator
         IntBinaryOperator intBinaryOperator = (int a, int b) -> {
             return a + b;
         };
 
-        intBinaryOperator.applyAsInt(2, 5);
+
+        // Lambda utilizando BiFunction
+        BiFunction<Integer, Integer, Integer> biFunctions = (Integer a, Integer b) -> {
+            return a + b;
+        };
+
+        // Passando valores a função IntBinaryIperator e armazenando na variável var1 para ser utilizada.
+        Integer var1 = intBinaryOperator.applyAsInt(5, 5);
+        Integer var2 = biFunctions.apply(10, 10);
+
+        System.out.println(var1);
+        System.out.println(var2);
+
+
     }
 }
